7f336c0ce40758b30209b6bc3233b9ec119a1096
Gabriela Leichtweis <gabrielaleichtweis@gmail.com>
2023-01-17 15:34:05 -0300

    pagina entrar rodando

--- entrar-sistema.js
+++ entrar-sistema.js
@@ -1,5 +1,5 @@
 const formularioEntrar = window.document.getElementById('form')
-console.log(formularioEntrar)
+const listaCadastros = buscarLocalStorage('cadastros')
 
 formularioEntrar.addEventListener('submit', (ev) => {
     ev.preventDefault()
@@ -7,20 +7,42 @@ formularioEntrar.addEventListener('submit', (ev) => {
     const inputEmail = window.document.getElementById('email')
     inputEmail.addEventListener('focus', () =>{
         feedbackHTML.innerHTML = ''
-        formularioEntrar.reset()
     })
-    console.log(inputEmail.value)
 
     const inputPassword = window.document.getElementById('password')
     inputPassword.addEventListener('focus', () =>{
         feedbackHTML.innerHTML = ''
-        formularioEntrar.reset()
     })
-    console.log(inputPassword.value)
 
     const feedbackHTML = window.document.getElementById('feedback')
 
-    if(inputEmail.value !== localStorage || inputPassword.value !== localStorage){
+    const existe = listaCadastros.find((valor) => valor.email === inputEmail.value && valor.password === inputPassword.value)
+
+    if(existe){
+        window.location.href = './recados.html'
+    } else {
         feedbackHTML.innerHTML = 'Opss! E-mail ou senha incorretos'
+        return
     }
+})
+
+
+function buscarLocalStorage(chave){
+    const dadosJSON = localStorage.getItem(chave)
+
+    if(dadosJSON){
+        const dadosConvertidos = JSON.parse(dadosJSON)
+        return dadosConvertidos
+    } else {
+        return []
+    }
+}
+
+
+const criarConta = document.getElementById('button-criar-conta')
+
+criarConta.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+
+    window.location.href = './criar-conta.html'
 })
